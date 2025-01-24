2ede3a8012965d6141c85924030dab24c7c367cc
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-01-24 20:07:06 -0300

    feat: login controller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using SalesEscord.DTOs;
+using SalesEscord.Interfaces;
+
+namespace SalesEscord.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginDTO dto)
+        {
+            var claims = await _userService.Login(dto.Email, dto.Password);
+
+            await HttpContext.SignInAsync(claims);
+
+            return LocalRedirect("/");
+        }
+    }
+}
---
+++ DTOs/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesEscord.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
