7f07f3484416a175c56f1497053d02b704996fd2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-01-24 20:06:36 -0300

    feat: login logic

---
+++ Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+using SalesEscord.Models.Persistance;
+
+namespace SalesEscord.Interfaces
+{
+    public interface IUserService
+    {
+        Task<ClaimsPrincipal> Login(string email, string password);
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication;
+using SalesEscord.Context;
+using SalesEscord.Exceptions;
+using SalesEscord.Interfaces;
+using SalesEscord.Models.Persistance;
+
+namespace SalesEscord.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly DatabaseContext _context;
+        private readonly DbSet<UserModel> _users;
+        private readonly IBtoaHandler _btoaHandler;
+
+        public UserService(DatabaseContext context, IBtoaHandler btoaHandler)
+        {
+            _context = context;
+            _users = _context.Set<UserModel>();
+            _btoaHandler = btoaHandler;
+        }
+
+        public async Task<ClaimsPrincipal> Login(string email, string password)
+        {
+            var encryptPassword = _btoaHandler.Encrypt(password);
+            
+            password = string.Empty;
+
+            var user = await _users.FirstOrDefaultAsync(u => 
+                u.Email.Equals(email.ToLower(), StringComparison.CurrentCultureIgnoreCase) 
+                && u.Password == encryptPassword.Trim());
+
+            if (user == null) {
+                throw new UnauthorizedException("User Not Found");
+            }
+
+            user.Password = string.Empty;
+
+            if (user.IsBlocked)
+            {
+                throw new ForbiddenException("User Is Blocked");
+            }
+
+            var claims = new List<Claim>()
+            {
+                new(ClaimTypes.Name, user.Name),
+                new(ClaimTypes.Role, user.Role)
+            };
+
+            var identityClaims = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identityClaims);
+
+            return principal;
+        }
+    }
+}
