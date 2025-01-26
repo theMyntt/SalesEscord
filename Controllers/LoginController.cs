19e8b6b441005b705c577ddbb9a783f449b7e155
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-01-26 20:14:39 -0300

    chore: logout route on controller

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SalesEscord.DTOs;
@@ -18,7 +19,18 @@ namespace SalesEscord.Controllers
 
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+                return LocalRedirect("/?page=1&limit=10");
+
             return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return LocalRedirect("/Login");
         }
 
         [Authorize]
