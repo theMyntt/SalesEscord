629b7c218d4ae2771699182bff5b830b40c25395
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-01-25 23:23:15 -0300

    ci: sales table migration

---
+++ Migrations/20250126022214_SalesTableMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesEscord.Migrations
+{
+    /// <inheritdoc />
+    public partial class SalesTableMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBL_SALES",
+                columns: table => new
+                {
+                    ID_SALE = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TX_PRODUCT_NAME = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TX_FINAL_PRICE = table.Column<double>(type: "float", nullable: false),
+                    TX_DATE_OF_SALE = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBL_SALES", x => x.ID_SALE);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBL_SALES");
+        }
+    }
+}
